8c671559cb914063eeaff2c8a99e6974e2bb0106
Barsukov Dmitrii <d.barsukov@matras.ru>
2024-10-20 16:40:08 +0400

    Add Vite environment declaration file

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+	<div>
+		<a href="https://vitejs.dev" target="_blank">
+			<img src="/vite.svg" class="logo" alt="Vite logo" />
+		</a>
+		<a href="https://vuejs.org/" target="_blank">
+			<img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+		</a>
+	</div>
+	<HelloWorld msg="Vite + Vue" />
+
+	<div class="q-pa-md q-gutter-md">
+		<q-btn color="teal" size="sm" label="Unread Mails">
+			<q-badge color="orange" floating transparent> ∞ </q-badge>
+		</q-btn>
+
+		<q-btn dense round flat icon="email">
+			<q-badge color="red" floating transparent> 4 </q-badge>
+		</q-btn>
+
+		<div class="text-h4">
+			Title
+			<q-badge transparent align="middle" color="orange"> app v3.0.0 </q-badge>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.logo {
+	height: 6em;
+	padding: 1.5em;
+	will-change: filter;
+	transition: filter 300ms;
+}
+.logo:hover {
+	filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+	filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+// FILE: vite.config.js
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [
+		vue({
+			template: { transformAssetUrls },
+		}),
+
+		// @quasar/plugin-vite options list:
+		// https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
+		quasar({
+			// sassVariables: './src/quasar-variables.sass',
+		}),
+		basicSsl(),
+	],
+	build: {
+		outDir: './docs',
+	},
+	base: './',
+})
