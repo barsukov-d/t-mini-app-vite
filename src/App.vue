cae16024239dfd150da4cf9884eb359738d137b8
Barsukov Dmitrii <d.barsukov@matras.ru>
2024-10-22 17:49:00 +0400

    Remove unused import and commented code

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 // import HelloWorld from './components/HelloWorld.vue'
 
-import WebApp from '@twa-dev/sdk'
+// import WebApp from '@twa-dev/sdk'
 import { ref } from 'vue'
 
 const tab = ref('mails')
