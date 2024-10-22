7567adf88aade879f564ce8cf486c4b35b3c21d8
Barsukov Dmitrii <d.barsukov@matras.ru>
2024-10-22 18:24:48 +0400

    Refactor header in App.vue component, use hash for router history, remove unused import and commented code, and add Home and Leaderboard pages

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,25 @@
 
 // import WebApp from '@twa-dev/sdk'
 import { ref } from 'vue'
+import { RouterView } from 'vue-router'
+import { useRouter } from 'vue-router'
 
 const tab = ref('mails')
+
+const router = useRouter()
+
+function navigateToLeaderboard() {
+	router.push('/leaderboard')
+}
 </script>
 
 <template>
 	<header>header !</header>
 	<main class="flex-shrink-0 flex-grow">
-		<router-view></router-view>
+		<RouterView></RouterView>
 	</main>
 	<footer>
+		<button @click="navigateToLeaderboard">Перейти в лидерборд</button>
 		<q-tabs v-model="tab" class="text-teal">
 			<q-tab name="mails" icon="mail" label="Mails" />
 			<q-tab name="alarms" icon="alarm" label="Alarms" />
